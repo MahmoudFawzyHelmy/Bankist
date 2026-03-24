0d9ca80d9a8cac8d2d9bb27cad844ce12b591577
MahmoudFawzyHelmy <mahmoudfawzy.helmy@gmail.com>
2026-03-24 17:03:17 +0200

    update scrolling

--- script.js
+++ script.js
@@ -30,3 +30,28 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+const btnScrollTo = document.querySelector('.btn--scroll-to')
+const section1 = document.querySelector('#section--1')
+
+btnScrollTo.addEventListener("click", function (e) {
+  // const s1coords = section1.getBoundingClientRect();
+  // console.log(s1coords);
+  // console.log(e.target.getBoundingClientRect());
+  // console.log('Current Scroll (X,Y)', window.scrollX, window.scrollY);
+  // console.log('Height / Width  ViewPort', document.documentElement.clientHeight, document.documentElement.clientWidth);
+  
+  //  Scrolling
+  // window.scrollTo(s1coords.left + window.scrollX , s1coords.top + window.scrollY)
+
+  // Scrolling Smothing
+//   window.scrollTo({
+//     left: s1coords.left + window.scrollX,
+//     top: s1coords.top + window.scrollY,
+//     behavior:"smooth"
+  // })
+  
+  section1.scrollIntoView({
+    behavior:"smooth"
+  })
+})
